145832111faacc9ef1e619fedc38644b7f62fbdb
= <=>
2023-07-14 00:49:16 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,22 +1,35 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
 
+
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)// nenhuma operação de escrita
+    public GameDTO findById(Long id){
+          Optional<GameDTO> result = gameRepository.findById(id).map(x -> new GameDTO(x));
+          return result.get();
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
+
+
 }
